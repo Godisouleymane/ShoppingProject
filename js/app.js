de572e142ef377ecc952d06d1693156309e570f7
Godisouleymane <ssabiougodi@gmail.com>
2023-09-12 20:30:47 +0100

    init

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ const categoryTitle = document.querySelectorAll('.category-title');
 const AllCategoryPosts = document.querySelectorAll('.all');
 const allCard = document.querySelectorAll('.card');
 const cartIcon = document.querySelectorAll(".cart-icon");
-
+const cartAmount = document.querySelector(".cartAmount");
+const cartTitle = document.querySelector(".cartTitle");
+const categoryName = document.querySelectorAll(".category-name");
+const prix = document.querySelectorAll(".amount");
+const AllDepense = document.querySelector(".AllDepense");
+const prixNom = document.querySelectorAll(".prix-nom");
+const mesImages = document.querySelectorAll(".card-img-top");
 
 for (let i = 0; i < categoryTitle.length; i++) {
   categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
@@ -57,7 +63,113 @@ for (let i = 0; i < AllCategoryPosts.length; i++) {
     AllCategoryPosts[i].addEventListener('mouseleave', () => {
         cartIcon[i].classList.add('hidden')
     })
-
-
-    
+   
 }
+
+
+
+cartIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        const item = icon.parentElement.parentElement;
+                const title = item.querySelectorAll('.category-name').textContent;
+                const price = item.querySelectorAll('.amount').textContent;
+                const image = item.querySelectorAll('.card-img-top').src;
+
+                const cartItem = document.createElement('div');
+                cartItem.classList.add('cart-item');
+                cartItem.innerHTML = `
+                <div class="d-flex justify-content-evenly">
+                    <div class="imgCart">
+                    <img src="${image}" alt="${title}">
+                    </div>
+                    <div class="d-flex flex-column ms-5">
+                        <span class="cartTitle fs-4">${title}</span>
+                        <span class="cartAmount">${price}</span>
+                    </div>
+                       <div class="mt-2">
+                        <button class="delete-button text-danger fs-3 border-0 ">
+                               <i class="bi bi-trash-fill"></i>
+                           </button>
+                    </div>
+                </div>
+                `;
+
+        AllDepense.appendChild(cartItem)
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function AjoutInCart() {
+//     for (let i = 0; i < prixNom.length; i++) {
+//         let name = categoryName.value
+//         let valeur = prix.value
+//         console.log(name);
+
+//         const div = document.createElement("div")
+//         div.innerHTML = `<div class="d-flex justify-content-evenly">
+//         <div class="imgCart">
+           
+//         </div>
+//         <div class="d-flex flex-column ms-5">
+//             <span class="cartTitle fs-4">${name}</span>
+//             <span class="cartAmount">${valeur}</span>
+//         </div>
+//         <div class="mt-2">
+//             <button class="delete-button text-danger fs-3 border-0 ">
+//                 <i class="bi bi-trash-fill"></i>
+//             </button>
+//         </div>
+//     </div>`
+        
+//     AllDepense.appendChild(div)
+//     }
+
+
+
+// }
+
+
+
+// for (let i = 0; i < cartIcon.length; i++) {
+//     cartIcon[i].addEventListener('click', (event) => {
+//         event.preventDefault();
+//         AjoutInCart();
+//     })
+    
+// }
